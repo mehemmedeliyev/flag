5a838cafc5fa4890913a37d5ca99a1ffc3303e84
mehemmedeliyev <79990609+mehemmedeliyev@users.noreply.github.com>
2021-03-27 12:24:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function press(){
+document.getElementById("flag1").innerHTML="Azerbaijan"
+};
+function press(){
+document.getElementById("flag2").innerHTML="Turkey"
+};
+function press(){
+document.getElementById("flag3").innerHTML="Berlin"
+};
+
